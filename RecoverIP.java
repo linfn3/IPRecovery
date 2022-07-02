abcc765adc7d2cd66542cca3d2c885a9a3ab7cfc
lin-fengnian <1511403142@qq.com>
2022-07-03 02:50:55 +0800

    second

--- RecoverIP.java
+++ RecoverIP.java
@@ -13,12 +13,31 @@ class RecoverIP {
         List<String> ans=new ArrayList<>();
         int len = s.length();
         if(len <=3 || len >= 13){
-            return ans;
+            return ans; //判断s是否可能构成ip
         }
-
-        dfs();
+        dfs(s, "", 0, 0,len,ans); //使用回溯法
         return ans;
     }
+    public static void dfs(String s, String temp, int area, int cut, int len, List<String> ans){
 
+
+
+        if(temp.length() - len == 4){
+            temp = temp.substring(0,temp.length()-1);
+            ans.add(temp);
+            return;
+        }
+        else if(area >= 4){
+            return;
+        }
+        else{
+            StringBuffer add_temp = new StringBuffer();
+            for(int i = 1;i <= 3 && cut+i-1<len;i++){
+                char tmp = s.charAt(cut+i-1);
+                add_temp.append(tmp);
+                dfs(s,temp+add_temp.toString()+'.', area+1,cut+i, len, ans);
+            }
+        }
+    }
 
 }
